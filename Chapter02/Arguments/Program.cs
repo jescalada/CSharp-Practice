04df5841035d42668c71932d14571f415de45c41
Juan Escalada <juanescalada175@gmail.com>
2022-05-12 18:08:30 -0700

    Add Arguments folder

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                WriteLine("You must specify two colors and cursor size, e.g.");
+                WriteLine("dotnet run red yellow 50");
+                return; // stop running 
+            }
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true);
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true);
+            CursorSize = int.Parse(args[2]);
+        }
+    }
+}
