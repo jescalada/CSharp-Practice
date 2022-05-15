4e5b03ccd6aa59b8d7ae3f12d646f47c12940e31
Juan Escalada <juanescalada175@gmail.com>
2022-05-15 13:41:42 -0700

    Update Program.cs

--- Chapter04/Functions/Program.cs
+++ Chapter04/Functions/Program.cs
@@ -37,9 +37,32 @@ namespace Functions
             } while (isNumber);
         }
 
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    int lastDigit = number % 10;
+
+                    string suffix = lastDigit switch
+                    {
+                        1 => "st",
+                        2 => "nd",
+                        3 => "rd"
+                    };
+                    return $"{number}{suffix}";
+            }
+        }
+
         static void Main(string[] args)
         {
             RunTimesTable();
+            CardinalToOrdinal(12);
+            CardinalToOrdinal(25);
         }
     }
 }
